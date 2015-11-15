806da5492a5fd58b18ce069c97c1cde3302b9417
James Tauber <jtauber@jtauber.com>
2015-11-14 22:34:26 -0500

    pull out lexemes as separate relation

--- make_json.py
+++ make_json.py
@@ -5,15 +5,23 @@ import json
 
 words = []
 forms = {}
+lexemes = {}
 
 with open("raw_data/john_3_a.txt") as f:
     for line in f:
         word_id, bcv, para_id, sent_id, pos, parse, crit, text, word, norm, lemma, rel, head = line.strip().split()
+
+        if (lemma, pos) not in lexemes:
+            lexeme_id = len(lexemes)
+            lexemes[(lemma, pos)] = lexeme_id
+        else:
+            lexeme_id = lexemes[(lemma, pos)]
+
         if (norm, lemma, pos, parse) not in forms:
             form_id = len(forms)
-            forms[(norm, lemma, pos, parse)] = form_id
+            forms[(norm, parse, lexeme_id)] = form_id
         else:
-            form_id = forms[(norm, lemma, pos, parse)]
+            form_id = forms[(norm, parse, lexeme_id)]
 
         words.append({
             "word_id": word_id,
@@ -26,13 +34,23 @@ with open("base.json", "w") as f:
 
 ordered_forms = []
 for t, form_id in sorted(forms.items(), key=lambda pair: pair[1]):
-    form, lemma, pos, parse = t
+    form, parse, lexeme_id = t
     ordered_forms.append({
         "form": form,
-        "lemma": lemma,
-        "pos": pos,
         "parse": parse,
+        "lexeme_id": lexeme_id,
     })
 
 with open("forms.json", "w") as f:
     json.dump(ordered_forms, f, indent=2, sort_keys=True, ensure_ascii=False)
+
+ordered_lexemes = []
+for t, lexeme_id in sorted(lexemes.items(), key=lambda pair: pair[1]):
+    lemma, pos = t
+    ordered_lexemes.append({
+        "lemma": lemma,
+        "pos": pos,
+    })
+
+with open("lexemes.json", "w") as f:
+    json.dump(ordered_lexemes, f, indent=2, sort_keys=True, ensure_ascii=False)
