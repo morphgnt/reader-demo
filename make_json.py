285b54b810d2d0fdddfe40fd3ed9aa1feed127f8
James Tauber <jtauber@jtauber.com>
2015-11-14 23:13:40 -0500

    humanize pos and parse

--- make_json.py
+++ make_json.py
@@ -3,6 +3,126 @@
 
 import json
 
+
+def humanize_pos(pos):
+    return {
+        "A-": "adjective",
+        "C-": "conjunction",
+        "D-": "adverb",
+        "N-": "noun",
+        "P-": "preposition",
+        "RA": "article",
+        "RD": "demonstrative",
+        "RI": "indefinite/interrogative pronoun",
+        "RP": "personal pronoun",
+        "RR": "relative pronoun",
+        "V-": "verb",
+        "X-": "particle",
+    }[pos]
+
+
+CASES = {
+    "N": "nominative",
+    "G": "genitive",
+    "D": "dative",
+    "A": "accusative",
+    "V": "vocative",
+}
+
+NUMBERS = {
+    "S": "singular",
+    "P": "plural",
+}
+
+GENDERS = {
+    "M": "masculine",
+    "F": "feminine",
+    "N": "neuter",
+    "-": "",
+}
+
+DEGREES = {
+    "-": "",
+}
+
+ASPECTS_TENSES = {
+    "A": "aorist",
+    "P": "present",
+    "F": "future",
+    "I": "imperfect",
+    "X": "perfect",
+}
+
+ASPECTS = {
+    "P": "continuous",
+    "A": "aorist",
+    "X": "perfect"
+}
+
+VOICES = {
+    "A": "active",
+    "M": "1st medio-passive",
+    "P": "2nd medio-passive",
+}
+
+MOODS = {
+    "I": "indicative",
+    "N": "infinitive",
+    "S": "subjunctive",
+    "P": "participle",
+}
+
+PERSONS = {
+    "1": "1st person",
+    "2": "2nd person",
+    "3": "3rd person",
+}
+
+
+def humanize_parse(pos, parse):
+    person, aspect_tense, voice, mood, case, number, gender, degree = parse
+    if pos in ["C-", "D-", "P-", "X-"]:
+        return ""
+    elif pos == "A-":
+        return " ".join([CASES[case], NUMBERS[number], GENDERS[gender], DEGREES[degree]])
+    elif pos in ["N-", "RA", "RD", "RI", "RP", "RR"]:
+        return " ".join([CASES[case], NUMBERS[number], GENDERS[gender]])
+    elif pos == "V-":
+        if aspect_tense not in "AF" and voice == "P":
+            voice = "M"
+        if mood in "DSO":
+            return " ".join([
+                ASPECTS[aspect_tense],
+                VOICES[voice],
+                MOODS[mood],
+                PERSONS[person],
+                NUMBERS[number]
+            ])
+        if mood == "I":
+            return " ".join([
+                ASPECTS_TENSES[aspect_tense],
+                VOICES[voice],
+                MOODS[mood],
+                PERSONS[person],
+                NUMBERS[number]
+            ])
+        elif mood == "P":
+            return " ".join([
+                ASPECTS[aspect_tense],
+                VOICES[voice],
+                MOODS[mood],
+                CASES[case],
+                NUMBERS[number],
+                GENDERS[gender]
+            ])
+        elif mood == "N":
+            return " ".join([
+                ASPECTS[aspect_tense],
+                VOICES[voice],
+                MOODS[mood]
+            ])
+
+
 words = []
 forms = {}
 lexemes = {}
@@ -19,9 +139,9 @@ with open("raw_data/john_3_a.txt") as f:
 
         if (norm, lemma, pos, parse) not in forms:
             form_id = len(forms)
-            forms[(norm, parse, lexeme_id)] = form_id
+            forms[(norm, pos, parse, lexeme_id)] = form_id
         else:
-            form_id = forms[(norm, parse, lexeme_id)]
+            form_id = forms[(norm, pos, parse, lexeme_id)]
 
         words.append({
             "word_id": word_id,
@@ -34,10 +154,10 @@ with open("base.json", "w") as f:
 
 ordered_forms = []
 for t, form_id in sorted(forms.items(), key=lambda pair: pair[1]):
-    form, parse, lexeme_id = t
+    form, pos, parse, lexeme_id = t
     ordered_forms.append({
         "form": form,
-        "parse": parse,
+        "parse": humanize_parse(pos, parse),
         "lexeme_id": lexeme_id,
     })
 
@@ -49,7 +169,7 @@ for t, lexeme_id in sorted(lexemes.items(), key=lambda pair: pair[1]):
     lemma, pos = t
     ordered_lexemes.append({
         "lemma": lemma,
-        "pos": pos,
+        "pos": humanize_pos(pos),
     })
 
 with open("lexemes.json", "w") as f:
