cf257f92a95c648ccdf5bbe2d3eecdf16f23e591
James Tauber <jtauber@jtauber.com>
2015-11-14 22:16:28 -0500

    build up form dictionary and form ids

--- make_base_json.py
+++ make_base_json.py
@@ -4,13 +4,21 @@
 import json
 
 words = []
+forms = {}
 
 with open("raw_data/john_3_a.txt") as f:
     for line in f:
         word_id, bcv, para_id, sent_id, pos, parse, crit, text, word, norm, lemma, rel, head = line.strip().split()
+        if (norm, lemma, pos, parse) not in forms:
+            form_id = len(forms)
+            forms[(norm, lemma, pos, parse)] = form_id
+        else:
+            form_id = forms[(norm, lemma, pos, parse)]
+
         words.append({
             "word_id": word_id,
             "text": text,
+            "form_id": form_id,
         })
 
 with open("base.json", "w") as f:
