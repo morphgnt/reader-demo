f3acd7f4bf4d601514f912614ce7adef446b09d4
James Tauber <jtauber@jtauber.com>
2015-11-14 22:00:16 -0500

    added first pass at base.json

---
+++ make_base_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+import json
+
+words = []
+
+with open("raw_data/john_3_a.txt") as f:
+    for line in f:
+        word_id, bcv, para_id, sent_id, pos, parse, crit, text, word, norm, lemma, rel, head = line.strip().split()
+        words.append({"text": text})
+
+with open("base.json", "w") as f:
+    json.dump(words, f)
